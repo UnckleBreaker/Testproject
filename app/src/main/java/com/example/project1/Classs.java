c57d2abb32085837a28dda83cd77d1702af23bd8
Valery Tihonovskiy <djigid994@gmail.com>
2020-05-14 11:45:58 +0300

    third commit

--- app/src/main/java/com/example/project1/Classs.java
+++ app/src/main/java/com/example/project1/Classs.java
@@ -1,6 +1,16 @@
 package com.example.project1;
 
-public class Classs {
- 
-    
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Classs extends AppCompatActivity {
+    String TAG = "Classs";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "asdfqeraesfdarfwefwefwefw");
+    }
 }
--- app/src/main/java/com/example/project1/MainActivity.java
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = "Activity";
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button =findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int cx =button.getWidth()/2;
+                int cy= button.getHeight()/2;
+                float radius = button.getWidth();
+                Animator animator = ViewAnimationUtils.createCircularReveal(button,cx,cy,radius,0);
+                animator.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        button.setVisibility(View.VISIBLE);
+                    }
+                });
+                animator.start();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
